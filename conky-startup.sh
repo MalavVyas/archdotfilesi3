1c7a4b8acc70d7880d1f6a73d1fe4b32befc26be
Malav Vyas <malavvyas98@gmail.com>
2019-03-21 16:31:44 -0700

    Add files via upload

---
+++ conky-startup.sh
@@ -0,0 +1,8 @@
+sleep 10s
+killall conky
+cd "/home/thelaw/.conky"
+conky -c "/home/thelaw/.conky/conkyrc0" &
+cd "/home/thelaw/.conky"
+conky -c "/home/thelaw/.conky/conkyrc1" &
+cd "/home/thelaw/.conky"
+conky -c "/home/thelaw/.conky/conkyrc2" &
